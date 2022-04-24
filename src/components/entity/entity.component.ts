200d56ae5bb0fb21b6cc7aa1e29cb48b44e4fb03
Sander <sander.nieuwenhuisen@gmail.com>
2022-04-24 23:37:40 +0200

    Started out with the entity stuff

---
+++ src/components/entity/entity.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'entity-component',
+  templateUrl: './entity.component.html',
+  styleUrls: ['./entity.component.scss'],
+})
+
+export class EntityComponent {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
